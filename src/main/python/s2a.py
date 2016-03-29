f298588181d0e790b01073b55f8ba36b8ef6f476
Monster <noreply@nowhere>
2016-03-29 21:24:52 +0200

    added tests
    added hitNormalToAlign

--- src/main/python/s2a.py
+++ src/main/python/s2a.py
@@ -8,26 +8,75 @@ to actuators, sets position or aligns to a vector
 __version__ = "2.0"
 __author__ = "Monster"
 
-import mbge
-import mutil
+from mbge import context
+from mutil import sensors
+
+PROPERTY_AXIS = "axis"; DEFAULT_AXIS = "X"
+PROPERTY_FACTOR = "factor"; DEFAULT_FACTOR = 1.0
+
+X_AXIS = 0
+Y_AXIS = 1
+Z_AXIS = 2
+
+AXIS_VALUE_TO_AXIS_MAPPING = {
+    "X": X_AXIS,
+    "Y": Y_AXIS,
+    "Z": Z_AXIS
+}
 
 def hitObjectToObject():
-    hitObjects = mutil.sensors.hitObjects
-    hitObject = min(hitObjects, key=lambda object: object.getDistanceTo(mbge.context.owner))
+    hitObject = _findClosestObject()
 
-    for actuator in mbge.context.controller.actuators:
+    for actuator in context.controller.actuators:
         try:
             actuator.object = hitObject
         except AttributeError:
             pass
 
+def hitObjectToTarget():
+    hitObject = _findClosestObject()
+
+    for actuator in context.controller.actuators:
+        try:
+            actuator.target = hitObject
+        except AttributeError:
+            pass
 
 def hitPositionToPosition():
-    hitObjects = mutil.sensors.hitObjects
-    hitObject = min(hitObjects, key=lambda object: object.getDistanceTo(mbge.context.owner))
+    hitObject = _findClosestObject()
+
     if not hitObject:
         return
 
-    for actuator in mbge.context.controller.actuators:
+    for actuator in context.controller.actuators:
         actuator.owner.worldPosition = hitObject.position
 
+def hitNormalToAlign():
+    sensor =_findSensorToClosestObject()
+
+    if not sensor:
+        return
+
+    hitNormal = sensor.hitObject
+    axisValue = context.owner.get(PROPERTY_AXIS, DEFAULT_AXIS)
+    axis = AXIS_VALUE_TO_AXIS_MAPPING[axisValue]
+    factor = context.owner.get(PROPERTY_FACTOR, DEFAULT_FACTOR)
+
+    for actuator in context.controller.actuators:
+        actuator.owner.alignAxisToVect(hitNormal.worldOrientation.col[0], axis, factor)
+
+def _findClosestObject():
+    hitObjects = sensors.hitObjects
+    return min(hitObjects, key=lambda object: object.getDistanceTo(context.owner))
+
+def _findSensorToClosestObject():
+    sensorToClosestObject = None
+    for sensor in context.controller.sensors:
+        try:
+            hitObject = sensor.hitObject
+            if (sensorToClosestObject or
+                hitObject.getDistanceTo(context.owner)<sensorToClosestObject.getDistanceTo(context.owner)):
+                sensorToClosestObject = sensor
+        except AttributeError:
+            continue
+    return sensor
